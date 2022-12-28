6c41615aeedb70527973c20778b2cf5fcb92be57
esponges <esponges@gmail.com>
2022-12-28 10:50:39 -0600

    home updates

--- src/components/atoms/pageHeader.tsx
+++ src/components/atoms/pageHeader.tsx
@@ -3,12 +3,12 @@ export const PageHeader = ({
   description,
 }: {
   title: string;
-  description: string;
+  description?: string;
 }) => {
   return (
     <div className="my-10 flex flex-col gap-4 rounded-xl p-4 text-white">
       <h3 className="text-4xl font-bold">{title}</h3>
-      <div className="text-lg">{description}</div>
+      {description && <div className="text-lg">{description}</div>}
     </div>
   );
 };
