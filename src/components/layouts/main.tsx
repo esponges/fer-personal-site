33eaa85ce3e8b7dbb27369f1af1c65dd44e26384
esponges <esponges@gmail.com>
2022-12-28 10:50:24 -0600

    change bg

--- src/components/layouts/main.tsx
+++ src/components/layouts/main.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "~/components/molecules/navbar";
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-gradient-to-b from-[#093679] to-[#00d4ff]">
       <Navbar />
       <main className="flex-grow">{children}</main>
     </div>
