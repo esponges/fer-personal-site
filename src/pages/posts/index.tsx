1d48fb9388b32f377647570a65c9ceb2db20f726
esponges <esponges@gmail.com>
2022-12-29 15:30:11 -0600

    improve posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,35 +1,38 @@
-import type { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import type {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from "next";
 
-import type { Post } from "~/types"; 
+import type { Post } from "~/types";
 
 import { PostCard } from "~/components/molecules/postCard";
 import { Container } from "~/components/organisms/container";
 import { PageHeader } from "~/components/atoms/pageHeader";
 
-const PostsPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const PostsPage = (
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) => {
   const posts = props.posts;
 
   return (
     <Container>
-      <div className="text-center mt-10">
-        <PageHeader
-          title="Posts"
-          description="I love to write about things I'm learning. 
+      <PageHeader
+        title="Posts"
+        description="I love to write about things I'm learning. 
           Blogging is a great way to improve and share knowledge.
           And who knows, maybe one day it might help me to write a book!"
-        />
-        <ul>
-          {posts?.map((post) => (
-            <li key={post.id}>
-              <PostCard
-                title={post.title}
-                description={post.description}
-                canonical_url={post.canonical_url}
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
+      />
+      <ul>
+        {posts?.map((post) => (
+          <li key={post.id}>
+            <PostCard
+              title={post.title}
+              description={post.description}
+              canonical_url={post.canonical_url}
+            />
+          </li>
+        ))}
+      </ul>
     </Container>
   );
 };
@@ -38,10 +41,10 @@ export default PostsPage;
 
 export const getServerSideProps = async (_ctx: GetServerSidePropsContext) => {
   // get the posts from the dev.to api
-  const url = 'https://dev.to/api/articles?username=esponges';
+  const url = "https://dev.to/api/articles?username=esponges";
 
   const res = await fetch(url);
-  const posts = await res.json() as Post[];
+  const posts = (await res.json()) as Post[];
 
   return {
     props: {
