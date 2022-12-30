8efb4fc4d805e26d755fc71497b145236a95c6f8
esponges <esponges@gmail.com>
2022-12-30 15:51:27 -0600

    reusable modal

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -21,3 +21,18 @@ export enum Routes {
   posts = '/posts',
   projects = '/projects',
 }
+
+export enum BgColor {
+  // tailwind colors
+  white = 'bg-white',
+  black = 'bg-black',
+  darkGray = 'bg-gray-900',
+  gray = 'bg-gray-100',
+  blue = 'bg-blue-100',
+  red = 'bg-red-100',
+  yellow = 'bg-yellow-100',
+  green = 'bg-green-100',
+  purple = 'bg-purple-100',
+  pink = 'bg-pink-100',
+  // custom colors
+}
