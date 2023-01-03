6c624cb4cbeb43f470b92e85ccb02264c9f5dbb3
esponges <esponges@gmail.com>
2023-01-03 17:13:58 -0600

    navbar to layouts

--- src/components/layouts/main.tsx
+++ src/components/layouts/main.tsx
@@ -1,10 +1,12 @@
-import { Navbar } from "~/components/molecules/navbar";
+import { Navbar } from "~/components/layouts/navbar";
+import { Footer } from "./footer";
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen bg-gradient-to-b from-[#093679] to-[#00d4ff] text-white">
       <Navbar />
       <main className="flex-grow">{children}</main>
+      <Footer />
     </div>
   );
 };
