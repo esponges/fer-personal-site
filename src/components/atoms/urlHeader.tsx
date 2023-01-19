b5e19e27559473c6c6afea8acb8fa4329f9d337a
esponges <esponges@gmail.com>
2023-01-19 16:49:51 -0600

    allow classnames in url

--- src/components/atoms/urlHeader.tsx
+++ src/components/atoms/urlHeader.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
-export const UrlHeader = ({ url }: { url: string }) => {
+export const UrlHeader = ({ url,  className = '' }: { url: string, className?: string }) => {
   return (
     <h2 className="text-2xl font-bold">
       <Link href={url} passHref>
-        <span className="underline hover:text-blue-600">{url}</span>
+        <span className={`underline hover:text-blue-600 ${className}`}>{url}</span>
       </Link>
     </h2>
   );
