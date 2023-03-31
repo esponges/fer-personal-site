0f1a4435e41da78fafbc3e8ced827a801135debf
esponges <esponges@gmail.com>
2023-03-31 15:58:24 -0600

    create regression test

---
+++ src/tests/e2e/regression.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+test('simple regression test', async ({ page }) => {
+  await page.goto('/');
+  await page.pause();
+
+  // home page
+  await page.getByRole('heading', { name: 'Hello 👋, I\'m Fernando' }).click();
+  await page.getByText('I\'m a software engineer from Guadalajara, Mexico. 🇲🇽 . I\'ve been coding since ').click();
+  await page.getByText('fernandogtostado@gmail.com').click();
+
+  // posts
+  await page.getByRole('link', { name: 'Posts' }).click();
+  await page.getByRole('heading', { name: 'Posts' }).click();
+
+  // grab the first html <article> element
+  const firstArticle = page.locator('article').first();
+  // expect first child element to be an <a> element
+  expect(firstArticle.locator('a').first()).toBeTruthy();
+
+  // my projects
+  await page.getByRole('link', { name: 'Projects', exact: true }).click();
+  await page.getByRole('heading', { name: 'Projects' }).click();
+  await page.getByText('A collection of projects I\'ve worked on.').click();
+
+  // open model click
+  await page.locator('img').first().click();
+  await page.getByRole('button', { name: 'Close' }).click();
+});
--- src/tests/e2e/test.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('basic test', async ({ page }) => {
-  await page.goto('/');
-  const title = page.locator('h1');
-  expect(await title.textContent()).toBe('Welcome to test!');
-});
-
