0fc7c3f63d9d853e543fd43e7a39fe95d824ae33
esponges <esponges@gmail.com>
2023-04-14 16:36:17 -0600

    refactor to use pagination hook

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,35 +1,23 @@
-import type {
-  GetServerSidePropsContext,
-  InferGetServerSidePropsType,
-} from "next";
-import Head from "next/head";
+import type { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import Head from 'next/head';
 
-import type { Post } from "~/types";
+import type { Post } from '~/types';
 
-import { PostCard } from "~/components/molecules/postCard";
-import { Container } from "~/components/organisms/container";
-import { PageHeader } from "~/components/atoms/pageHeader";
-import { Pagination } from "~/components/molecules/pagination";
-import { useState } from "react";
+import { PostCard } from '~/components/molecules/postCard';
+import { Container } from '~/components/organisms/container';
+import { PageHeader } from '~/components/atoms/pageHeader';
+import { Pagination } from '~/components/molecules/pagination';
+import { usePagination } from '~/utils/hooks/usePagination';
 
 const POSTS_PER_PAGE = 4;
 
-const PostsPage = (
-  props: InferGetServerSidePropsType<typeof getServerSideProps>
-) => {
+const PostsPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const { posts } = props;
 
-  const [postsToShow, setPostsToShow] = useState<Post[]>(posts.slice(0, 4));
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const handlePageChange = (page: number) => {
-    const start = (page - 1) * POSTS_PER_PAGE;
-    const end = start + POSTS_PER_PAGE;
-
-    setPostsToShow(posts.slice(start, end));
-    setCurrentPage(page);
-  };
-  const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE);
+  const { toShow, currentPage, totalPages, handlePageChange } = usePagination<Post>({
+    elementsPerPage: POSTS_PER_PAGE,
+    elements: posts,
+  });
 
   return (
     <>
@@ -47,17 +35,21 @@ const PostsPage = (
             Blogging is a great way to improve and share knowledge.
             And who knows, maybe one day it might help me to write a book!"
         />
-        {postsToShow?.map((post) => (
-          <article key={post.id}>
-            <PostCard
-              title={post.title}
-              description={post.description}
-              canonical_url={post.canonical_url}
-              cover_image={post.cover_image}
-              social_image={post.social_image}
-            />
-          </article>
-        ))}
+        {!!toShow.length ? (
+          toShow.map((post) => (
+            <article key={post.id}>
+              <PostCard
+                title={post.title}
+                description={post.description}
+                canonical_url={post.canonical_url}
+                cover_image={post.cover_image}
+                social_image={post.social_image}
+              />
+            </article>
+          ))
+        ) : (
+          <p className="text-center text-gray-500">No posts found. Please try again later.</p>
+        )}
         <Pagination
           page={currentPage}
           totalPages={totalPages}
@@ -72,7 +64,7 @@ export default PostsPage;
 
 export const getServerSideProps = async (_ctx: GetServerSidePropsContext) => {
   // get the posts from the dev.to api
-  const url = "https://dev.to/api/articles?username=esponges";
+  const url = 'https://dev.to/api/articles?username=esponges';
 
   const res = await fetch(url);
   const posts = (await res.json()) as Post[];
