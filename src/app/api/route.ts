c2ebc4f101283d7a65d1f0628ae1ac847f13c46e
esponges <esponges@gmail.com>
2023-05-16 18:49:26 -0600

    use app directory routing WIP

---
+++ src/app/api/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  // Do whatever you want
+  return new Response('Hello World!', {
+    status: 200,
+  });
+}
