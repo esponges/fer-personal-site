174d0563f95a3e621f7d7e82d4147edf783d72c1
esponges <esponges@gmail.com>
2023-06-03 11:40:20 -0600

    update drizzle schema

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,95 +1,210 @@
-import { pgTable, serial, uniqueIndex, timestamp, text, integer, numeric } from 'drizzle-orm/pg-core';
+import { pgTable, serial, uniqueIndex, varchar, timestamp, text, integer, numeric } from 'drizzle-orm/pg-core';
 
 /* 
     TODO:
-    Create relationships between tables
-    https://orm.drizzle.team/docs/rqb#declaring-relations
+    Confirm this works?
+    Inspired from: https://github.com/drizzle-team/drizzle-orm/issues/468
 */
 
-// CREATE TABLE public."Project" (
-//     id STRING NOT NULL,
-//     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT current_timestamp():::TIMESTAMP,
-//     "updatedAt" TIMESTAMP(3) NOT NULL,
-//     name STRING NOT NULL,
-//     subheader STRING NULL,
-//     description STRING NULL,
-//     url STRING NULL,
-//     tags STRING NULL,
-//     relevance FLOAT8 NULL DEFAULT 1.0:::FLOAT8,
-//     "repoUrl" STRING NULL,
-//     CONSTRAINT "Project_pkey" PRIMARY KEY (id ASC)
-//   )
+// model Account {
+//     id                String  @id @default(cuid())
+//     userId            String
+//     type              String
+//     provider          String
+//     providerAccountId String
+//     refresh_token     String? // @db.Text
+//     access_token      String? // @db.Text
+//     expires_at        Int?
+//     token_type        String?
+//     scope             String?
+//     id_token          String? // @db.Text
+//     session_state     String?
+//     user              User    @relation(fields: [userId], references: [id], onDelete: Cascade)
 
-export const project = pgTable('project', {
+//     @@unique([provider, providerAccountId])
+//   }
+
+export const accounts = pgTable(
+  'accounts',
+  {
     id: serial('id').primaryKey(),
-    createdAt: timestamp('created_at').notNull(),
-    updatedAt: timestamp('updated_at').notNull(),
-    name: text('name').notNull(),
+    userId: varchar('user_id', { length: 255 }),
+    type: text('type'),
+    provider: varchar('provider', { length: 50 }), // Changed to VARCHAR with length 50
+    providerAccountId: varchar('provider_account_id', { length: 100 }), // Changed to VARCHAR with length 100
+    refresh_token: text('refresh_token'),
+    access_token: text('access_token'),
+    expires_at: timestamp('expires_at'),
+    token_type: text('token_type'),
+    scope: text('scope'),
+    id_token: text('id_token'),
+    session_state: text('session_state'),
+  },
+  (accounts) => ({
+    userIdIndex: uniqueIndex('user_id_idx').on(accounts.userId),
+    providerIndex: uniqueIndex('provider_idx').on(accounts.provider),
+    providerAccountIdIndex: uniqueIndex('provider_account_id_idx').on(accounts.providerAccountId),
+  })
+);
+
+// model User {
+//     id            String    @id @default(cuid())
+//     name          String?
+//     email         String?   @unique
+//     emailVerified DateTime?
+//     image         String?
+//     accounts      Account[]
+//     sessions      Session[]
+//   }
+
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  name: text('name'),
+  email: text('email'),
+  emailVerified: timestamp('email_verified'),
+  image: text('image'),
+});
+
+// model Session {
+//     id           String   @id @default(cuid())
+//     sessionToken String   @unique
+//     userId       String
+//     expires      DateTime
+//     user         User     @relation(fields: [userId], references: [id], onDelete: Cascade)
+//   }
+
+export const sessions = pgTable(
+  'sessions',
+  {
+    id: serial('id').primaryKey(),
+    sessionToken: text('session_token'),
+    userId: text('user_id'),
+    expires: timestamp('expires'),
+  },
+  (sessions) => ({
+    sessionTokenIndex: uniqueIndex('session_token_idx').on(sessions.sessionToken),
+    // to confirm
+    userIdIndex: uniqueIndex('user_id_idx').on(sessions.userId),
+  })
+);
+
+// model VerificationToken {
+//     identifier String
+//     token      String   @unique
+//     expires    DateTime
+
+//     @@unique([identifier, token])
+//   }
+
+export const verificationTokens = pgTable(
+  // to confirm or verificationTokens label
+  'verification_tokens',
+  {
+    identifier: text('identifier'),
+    token: text('token'),
+    expires: timestamp('expires'),
+  },
+  // to confirm
+  (verificationTokens) => ({
+    identifierTokenIndex: uniqueIndex('identifier_token_idx').on(
+      verificationTokens.identifier,
+      verificationTokens.token
+    ),
+  })
+);
+
+// model Image {
+//     id          String   @id @default(cuid())
+//     createdAt   DateTime @default(now())
+//     updatedAt   DateTime @updatedAt
+//     url         String?
+//     alt         String
+//     title       String?
+//     placeholder String?
+//     width       Int?
+//     height      Int?
+//     // imagekey relative path
+//     path        String?
+//     ytUrl       String?
+//     Project     Project? @relation(fields: [projectId], references: [id])
+//     projectId   String?
+//   }
+
+export const images = pgTable(
+  'images',
+  {
+    id: serial('id').primaryKey(),
+    createdAt: timestamp('created_at'),
+    updatedAt: timestamp('updated_at'),
+    url: text('url'),
+    alt: text('alt'),
+    title: text('title'),
+    placeholder: text('placeholder'),
+    width: integer('width'),
+    height: integer('height'),
+    path: text('path'),
+    ytUrl: text('yt_url'),
+    projectId: text('project_id'),
+  },
+  (images) => ({
+    projectIdIndex: uniqueIndex('project_id_idx').on(images.projectId),
+  })
+);
+
+// model Lib {
+//     id        String   @id @default(cuid())
+//     createdAt DateTime @default(now())
+//     updatedAt DateTime @updatedAt
+//     name      String
+//     url       String
+//     // many to many project to lib
+//     Project   Project[]
+//   }
+
+export const libs = pgTable(
+  'libs',
+  {
+    id: serial('id').primaryKey(),
+    createdAt: timestamp('created_at'),
+    updatedAt: timestamp('updated_at'),
+    name: text('name'),
+    url: text('url'),
+  },
+  (libs) => ({
+    nameIndex: uniqueIndex('name_idx').on(libs.name),
+  })
+);
+
+// model Project {
+//     id          String   @id @default(cuid())
+//     createdAt   DateTime @default(now())
+//     updatedAt   DateTime @updatedAt
+//     name        String
+//     subheader   String?
+//     description String?
+//     url         String?
+//     tags        String?
+//     relevance   Float?  @default(1)
+//     images      Image[]
+//     libs        Lib[]
+//     repoUrl     String?
+//   }
+
+export const projects = pgTable(
+  'projects',
+  {
+    id: serial('id').primaryKey(),
+    createdAt: timestamp('created_at'),
+    updatedAt: timestamp('updated_at'),
+    name: text('name'),
     subheader: text('subheader'),
     description: text('description'),
     url: text('url'),
     tags: text('tags'),
     relevance: numeric('relevance'),
     repoUrl: text('repo_url'),
-}, (project) => {
-    return {
-        uniqueIndex: uniqueIndex('project_pkey').on(project.id),
-    };
-});
-
-// CREATE TABLE public."Lib" (
-//     id STRING NOT NULL,
-//     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT current_timestamp():::TIMESTAMP,
-//     "updatedAt" TIMESTAMP(3) NOT NULL,
-//     name STRING NOT NULL,
-//     url STRING NOT NULL,
-//     CONSTRAINT "Lib_pkey" PRIMARY KEY (id ASC)
-//   )
-
-export const lib = pgTable('lib', {
-    id: serial('id').primaryKey(),
-    createdAt: timestamp('created_at').notNull(),
-    updatedAt: timestamp('updated_at').notNull(),
-    name: text('name').notNull(),
-    url: text('url').notNull(),
-}, (lib) => {
-    return {
-        uniqueIndex: uniqueIndex('lib_pkey').on(lib.id),
-    };
-});
-
-// CREATE TABLE public."Image" (
-//     id STRING NOT NULL,
-//     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT current_timestamp():::TIMESTAMP,
-//     "updatedAt" TIMESTAMP(3) NOT NULL,
-//     url STRING NULL,
-//     alt STRING NOT NULL,
-//     title STRING NULL,
-//     placeholder STRING NULL,
-//     width INT4 NULL,
-//     height INT4 NULL,
-//     path STRING NULL,
-//     "projectId" STRING NULL,
-//     "ytUrl" STRING NULL,
-//     CONSTRAINT "Image_pkey" PRIMARY KEY (id ASC),
-//     CONSTRAINT "Image_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES public."Project"(id) ON DELETE SET NULL ON UPDATE CASCADE
-//   )
-
-export const image = pgTable('image', {
-    id: serial('id').primaryKey(),
-    createdAt: timestamp('created_at').notNull(),
-    updatedAt: timestamp('updated_at').notNull(),
-    url: text('url'),
-    alt: text('alt').notNull(),
-    title: text('title'),
-    placeholder: text('placeholder'),
-    width: integer('width'),
-    height: integer('height'),
-    path: text('path'),
-    projectId: text('projectId'),
-    ytUrl: text('ytUrl'),
-}, (image) => {
-    return {
-        uniqueIndex: uniqueIndex('image_pkey').on(image.id),
-    };
-});
+  },
+  (projects) => ({
+    nameIndex: uniqueIndex('name_idx').on(projects.name),
+  })
+);
