ea0ff645c878242f91ac7bfef617c4849b8e0183
esponges <esponges@gmail.com>
2023-06-06 16:26:24 -0600

    improve nav

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -1,29 +1,58 @@
 /* eslint-disable max-len */
-import Link from "next/link";
-import { Routes } from "~/types/enums";
-import { SocialMediaIcon } from "../atoms/socialMediaIcon";
+import Link from 'next/link';
+import { Routes } from '~/types/enums';
+import { SocialMediaIcon } from '../atoms/socialMediaIcon';
+
+const NavbarLink = ({
+  href,
+  children,
+  textSize = 'text-lg',
+  className,
+}: {
+  href: string;
+  children: React.ReactNode;
+  textSize?: string;
+  className?: string;
+}) => {
+  return (
+    <Link
+      href={href}
+      className={`ml-6`}
+    >
+      <span className={`rounded p-3 font-bold text-white ${textSize} ${className}`}>
+        {children}
+      </span>
+    </Link>
+  );
+};
 
 export const Navbar = () => {
+  const navElHoverClass = 'hover:bg-white/20 hover:text-gray';
+
   return (
     <nav className="flex h-20 flex-row items-center bg-white/10 px-2 md:px-0">
       <div className="w-full">
         <div className="mx-auto flex max-w-3xl justify-between">
           <div className="">
-            <Link href={Routes.home}>
-              <span className="text-xl font-bold text-white md:text-3xl">
-                FerTostado
-              </span>
-            </Link>
-            <Link href={Routes.posts}>
-              <span className="ml-6 font-bold text-white md:text-lg">
-                Posts
-              </span>
-            </Link>
-            <Link href={Routes.projects}>
-              <span className="ml-6 font-bold text-white md:text-lg">
-                Projects
-              </span>
-            </Link>
+            <NavbarLink
+              href={Routes.home}
+              textSize="text-3xl"
+              className={navElHoverClass}
+            >
+              FerTostado
+            </NavbarLink>
+            <NavbarLink
+              href={Routes.posts}
+              className={navElHoverClass}
+            >
+              Posts
+            </NavbarLink>
+            <NavbarLink
+              href={Routes.projects}
+              className={navElHoverClass}
+            >
+              Projects
+            </NavbarLink>
           </div>
           <div className="flex flex-row items-center gap-4">
             <SocialMediaIcon
