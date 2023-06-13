225ad938303f665dbd4f9e457b0982629ed205ae
esponges <esponges@gmail.com>
2023-06-12 18:19:12 -0600

    create navbar atoms

---
+++ src/components/atoms/navbar/navbarLink.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+export const NAV_LINK_HOVER_CLASS = 'hover:bg-white/20 hover:text-gray';
+
+export const NavbarLink = ({
+  href,
+  children,
+  textSize = 'text-lg',
+  className,
+  shouldDisplay = true,
+  isMobile = false,
+  textColor = 'text-white',
+  onClick,
+}: {
+  href: string;
+  children: React.ReactNode;
+  textSize?: string;
+  textColor?: string;
+  className?: string;
+  shouldDisplay?: boolean;
+  isMobile?: boolean;
+  onClick?: () => void;
+}) => {
+  return (
+    <Link
+      href={href}
+      onClick={onClick}
+      className={!isMobile && shouldDisplay ? 'ml-6' : 'block'}
+    >
+      <span
+        className={`rounded font-bold md:p-3 ${textColor} ${textSize} ${className} ${!shouldDisplay ? 'hidden' : null}`}
+      >
+        {children}
+      </span>
+    </Link>
+  );
+};
