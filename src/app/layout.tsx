d6d9f6bf08f2de261976c0069a93f3da208149d0
esponges <esponges@gmail.com>
2023-06-12 18:02:13 -0600

    toggle dark moc poc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,15 @@ import "~/styles/globals.css";
 import "react-awesome-slider/dist/styles.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  // use a combination of slate colors to create a gradient
+  const darkModeGradient = 'from-[#1e293b] to-[#0f172a]';
+  // bg-gradient-to-b from-[#093679] to-[#00d4ff]
+  const lightModeGradient = 'from-[#093679] to-[#00d4ff]';
+
   return (
     <html lang='EN'>
       <head />
-      <body className="flex min-h-screen flex-col bg-gradient-to-b from-[#093679] to-[#00d4ff] text-white">
+      <body className={`flex min-h-screen flex-col bg-blue-900 dark:bg-gray-800 text-white`}>
         <Navbar />
         <main className="flex-grow">{children}</main>
         <Footer />
