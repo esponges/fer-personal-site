66e5161ae7615744ae046daa6461feb0f064f1ae
esponges <esponges@gmail.com>
2023-06-13 18:23:53 -0600

    Revert "no navbar"
    This reverts commit 788e44d29d8b1798ba79007260da809eb20dcb94.

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang='EN'>
       <head />
       <body className={`flex min-h-screen flex-col bg-blue-900 dark:bg-gray-800 font-color--primary`}>
-        {/* <Navbar /> */}
+        <Navbar />
         <main className="flex-grow">{children}</main>
         <Footer />
       </body>
