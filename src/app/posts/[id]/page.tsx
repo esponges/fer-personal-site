34291b053f5f01aca2be2d3335bf317e21fa5477
esponges <esponges@gmail.com>
2023-07-08 12:49:23 -0600

    custom elements

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,15 +1,63 @@
-import { PageHeader } from "~/components/atoms/pageHeader";
 import { Container } from "~/components/organisms/container";
 import { getPostDetails } from "~/utils/posts";
 
-import Markdown from 'markdown-to-jsx';
+import Markdown from "markdown-to-jsx";
+
+const CustomElement = ({ children, type, ...props }: { children: React.ReactNode; type: 'strong' | 'p' | 'pre' | 'code'; props: HTMLElement }) => {
+
+  switch (type) {
+    case 'strong':
+      return <strong {...props}>{children}</strong>;
+    case 'p':
+      return <p {...props}>{children}</p>;
+    case 'pre':
+      return <pre {...props}>{children}</pre>;
+    case 'code':
+      return <code {...props}>{children}</code>;
+    default:
+      return <p {...props}>{children}</p>;
+  }
+};
 
 export default async function PostDetails({ params }: { params: { id: string } }) {
-  const { body_markdown: markdown, body_html: html } = await getPostDetails(params.id);
+  const { body_markdown: markdown } = await getPostDetails(params.id);
 
   return (
-    <Container>
-      <Markdown>
+    <Container textCenter={false}>
+      <Markdown
+        options={{
+          overrides: {
+            p: {
+              component: CustomElement,
+              props: {
+                className: "text-md leading-7 my-4 md:my-6 dark:text-gray-300",
+                type: 'p',
+              },
+            },
+            pre: {
+              component: CustomElement,
+              props: {
+                className: "rounded-md bg-gray-700 text-white p-4 my-4 md:my-6",
+                type: 'pre',
+              },
+            },
+            code: {
+              component: CustomElement,
+              props: {
+                className: "text-sm bg-gray-700 text-white p-1 rounded-md",
+                type: 'code',
+              },
+            },
+            strong: {
+              component: CustomElement,
+              props: {
+                className: "font-bold text-lg leading-7 my-4 md:my-6 dark:text-gray-300",
+                type: 'strong',
+              },
+            },
+          },
+        }}
+      >
         {markdown}
       </Markdown>
     </Container>
--- src/components/organisms/container.tsx
+++ src/components/organisms/container.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const Container = ({ children }: { children: React.ReactNode }) => {
+export const Container = ({ children, textCenter = true }: { children: React.ReactNode; textCenter: boolean }) => {
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div className="max-w-3xl mx-auto mt-10">{children}</div>
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className={`mx-auto mt-10 max-w-3xl ${textCenter ? "text-center" : ""}`}>{children}</div>
     </div>
   );
 };
