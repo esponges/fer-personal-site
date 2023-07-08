a9f25e48ebee443fe04a312aaa6739b715f2625e
esponges <esponges@gmail.com>
2023-07-08 13:03:48 -0600

    custom element styling

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,21 +2,16 @@ import { Container } from "~/components/organisms/container";
 import { getPostDetails } from "~/utils/posts";
 
 import Markdown from "markdown-to-jsx";
+import { createElement } from "react";
 
-const CustomElement = ({ children, type, ...props }: { children: React.ReactNode; type: 'strong' | 'p' | 'pre' | 'code'; props: HTMLElement }) => {
+type CustomElementProps = {
+  children: React.ReactNode;
+  type: keyof HTMLElementTagNameMap;
+  props: HTMLElement;
+};
 
-  switch (type) {
-    case 'strong':
-      return <strong {...props}>{children}</strong>;
-    case 'p':
-      return <p {...props}>{children}</p>;
-    case 'pre':
-      return <pre {...props}>{children}</pre>;
-    case 'code':
-      return <code {...props}>{children}</code>;
-    default:
-      return <p {...props}>{children}</p>;
-  }
+const CustomElement = ({ children, type, ...props }: CustomElementProps) => {
+  return createElement(type, props, children);
 };
 
 export default async function PostDetails({ params }: { params: { id: string } }) {
@@ -31,28 +26,70 @@ export default async function PostDetails({ params }: { params: { id: string } }
               component: CustomElement,
               props: {
                 className: "text-md leading-7 my-4 md:my-6 dark:text-gray-300",
-                type: 'p',
+                type: "p",
               },
             },
             pre: {
               component: CustomElement,
               props: {
-                className: "rounded-md bg-gray-700 text-white p-4 my-4 md:my-6",
-                type: 'pre',
+                className: "rounded-md bg-gray-700 text-white p-4 my-4 md:my-6 overflow-x-auto",
+                type: "pre",
               },
             },
             code: {
               component: CustomElement,
               props: {
                 className: "text-sm bg-gray-700 text-white p-1 rounded-md",
-                type: 'code',
+                type: "code",
               },
             },
             strong: {
               component: CustomElement,
               props: {
                 className: "font-bold text-lg leading-7 my-4 md:my-6 dark:text-gray-300",
-                type: 'strong',
+                type: "strong",
+              },
+            },
+            img: {
+              component: CustomElement,
+              props: {
+                className: "rounded-md my-4 md:my-6",
+                type: "img",
+              },
+            },
+            ul: {
+              component: CustomElement,
+              props: {
+                className: "list-disc list-inside my-4 md:my-6",
+                type: "ul",
+              },
+            },
+            a: {
+              component: CustomElement,
+              props: {
+                className: "text-blue-500 underline hover:text-blue-600",
+                type: "a",
+              },
+            },
+            h1: {
+              component: CustomElement,
+              props: {
+                className: "text-3xl font-bold leading-7 my-4 md:my-6 dark:text-gray-300",
+                type: "h1",
+              },
+            },
+            h2: {
+              component: CustomElement,
+              props: {
+                className: "text-2xl font-bold leading-7 my-4 md:my-6 dark:text-gray-300",
+                type: "h2",
+              },
+            },
+            h3: {
+              component: CustomElement,
+              props: {
+                className: "text-xl font-bold leading-7 my-4 md:my-6 dark:text-gray-300",
+                type: "h3",
               },
             },
           },
