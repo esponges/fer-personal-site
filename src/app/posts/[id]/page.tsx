6bc14f5f66c24b192e745d0dd63815bbe872375e
esponges <esponges@gmail.com>
2023-07-08 12:20:27 -0600

    markdown to jsx works better with indentation

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,16 +1,17 @@
-import ReactMarkdown from "react-markdown";
-import rehypeRaw from "rehype-raw";
-import remarkGfm from "remark-gfm";
 import { PageHeader } from "~/components/atoms/pageHeader";
 import { Container } from "~/components/organisms/container";
 import { getPostDetails } from "~/utils/posts";
+
+import Markdown from 'markdown-to-jsx';
 
 export default async function PostDetails({ params }: { params: { id: string } }) {
   const { body_markdown: markdown, body_html: html } = await getPostDetails(params.id);
 
   return (
     <Container>
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
+      <Markdown>
+        {markdown}
+      </Markdown>
     </Container>
   );
 }
--- src/components/organisms/container.tsx
+++ src/components/organisms/container.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Container = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div className="max-w-3xl mx-auto text-center mt-10">{children}</div>
+      <div className="max-w-3xl mx-auto mt-10">{children}</div>
     </div>
   );
 };
