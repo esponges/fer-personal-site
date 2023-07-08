c81a4bebbfe0309b23a080e570db9b41dfdf36d5
esponges <esponges@gmail.com>
2023-07-08 10:00:48 -0600

    fetch details

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -9,3 +9,12 @@ export const getPosts = async () => {
   // order the posts by positive_reactions_count
   return posts.sort((a, b) => b.positive_reactions_count - a.positive_reactions_count);
 };
+
+export const getPostDetails = async (id: string) => {
+  const url = `https://dev.to/api/articles/${id}`;
+
+  const res = await fetch(url);
+  const post = (await res.json()) as Post<true>;
+
+  return post;
+}
