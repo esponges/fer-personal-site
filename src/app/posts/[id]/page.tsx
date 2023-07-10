434c52ee2b042a063e5539cf360872db1f1f2cd1
esponges <esponges@gmail.com>
2023-07-10 14:07:51 -0600

    refactor overrides

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -11,10 +11,26 @@ type CustomElementProps = {
   props: HTMLElement;
 };
 
+const overrideCustomElement = (type: keyof HTMLElementTagNameMap, className: string) => {
+  const language = className.replace("lang-", "");
+  console.log(language);
+
+  return {
+    component: CustomElement,
+    props: {
+      className,
+      type,
+    },
+  };
+};
+
 const CustomElement = ({ children, type, ...props }: CustomElementProps) => {
   return createElement(type, props, children);
 };
 
+// todo: figure out if there's a way to use the slug instead of the id
+// not sure if possible in RSCs directly yet
+// id prefer this router to be posts/[slug]?id=123
 export default async function PostDetails({ params }: { params: { id: string } }) {
   const { body_markdown: markdown, title } = await getPostDetails(params.id);
 
@@ -24,76 +40,16 @@ export default async function PostDetails({ params }: { params: { id: string } }
       <Markdown
         options={{
           overrides: {
-            p: {
-              component: CustomElement,
-              props: {
-                className: "text-md leading-7 my-4 md:my-6 dark:text-gray-300",
-                type: "p",
-              },
-            },
-            pre: {
-              component: CustomElement,
-              props: {
-                className: "rounded-md bg-gray-700 text-white p-4 my-4 md:my-6 overflow-x-auto",
-                type: "pre",
-              },
-            },
-            code: {
-              component: CustomElement,
-              props: {
-                className: "text-sm bg-gray-700 text-white p-1 rounded-md",
-                type: "code",
-              },
-            },
-            strong: {
-              component: CustomElement,
-              props: {
-                className: "font-bold text-lg leading-7 my-4 md:my-6 dark:text-gray-300",
-                type: "strong",
-              },
-            },
-            img: {
-              component: CustomElement,
-              props: {
-                className: "rounded-md my-4 md:my-6 mx-auto",
-                type: "img",
-              },
-            },
-            ul: {
-              component: CustomElement,
-              props: {
-                className: "list-disc list-inside my-4 md:my-6",
-                type: "ul",
-              },
-            },
-            a: {
-              component: CustomElement,
-              props: {
-                className: "text-blue-500 underline hover:text-blue-600",
-                type: "a",
-              },
-            },
-            h1: {
-              component: CustomElement,
-              props: {
-                className: "text-3xl font-bold leading-7 my-4 md:my-6 dark:text-gray-300",
-                type: "h1",
-              },
-            },
-            h2: {
-              component: CustomElement,
-              props: {
-                className: "text-2xl font-bold leading-7 my-4 md:my-6 dark:text-gray-300",
-                type: "h2",
-              },
-            },
-            h3: {
-              component: CustomElement,
-              props: {
-                className: "text-xl font-bold leading-7 my-4 md:my-6 dark:text-gray-300",
-                type: "h3",
-              },
-            },
+            p: overrideCustomElement("p", "text-md leading-7 my-4 md:my-6 dark:text-gray-300"),
+            pre: overrideCustomElement("pre", "rounded-md bg-gray-700 text-white p-4 my-4 md:my-6 overflow-x-auto"),
+            code: overrideCustomElement("code", "text-sm bg-gray-700 text-white p-1 rounded-md"),
+            strong: overrideCustomElement("strong", "font-bold text-lg leading-7 my-4 md:my-6 dark:text-gray-300"),
+            img: overrideCustomElement("img", "rounded-md my-4 md:my-6 mx-auto"),
+            ul: overrideCustomElement("ul", "list-disc list-inside my-4 md:my-6"),
+            a: overrideCustomElement("a", "text-blue-500 underline hover:text-blue-600"),
+            h1: overrideCustomElement("h1", "text-3xl font-bold leading-7 my-4 md:my-6 dark:text-gray-300"),
+            h2: overrideCustomElement("h2", "text-2xl font-bold leading-7 my-4 md:my-6 dark:text-gray-300"),
+            h3: overrideCustomElement("h3", "text-xl font-bold leading-7 my-4 md:my-6 dark:text-gray-300"),
           },
         }}
       >
