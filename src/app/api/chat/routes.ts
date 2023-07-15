6730a2905c14c005c9a86d064a210aa46412c76b
esponges <esponges@gmail.com>
2023-07-15 14:53:10 -0600

    langchain imports

--- src/app/api/chat/routes.ts
+++ src/app/api/chat/routes.ts
@@ -1,14 +1,17 @@
-import type { ChainValues } from 'langchain/dist/schema';
-import { type NextRequest, NextResponse } from 'next/server';
+import type { ChainValues } from "langchain/dist/schema";
+import { type NextRequest, NextResponse } from "next/server";
+import { AIChatMessage, HumanChatMessage } from "langchain/schema";
+import type { BaseChatMessage } from "langchain/schema";
+import { HNSWLib } from 'langchain/vectorstores/hnswlib';
 
 // import { makeChain } from '~/utils/langchain';
 // import { pinecone } from '~/utils/pinecone';
 
-import type { Document } from 'langchain/document';
+import type { Document } from "langchain/document";
 
 interface RequestBody {
   question: string;
-  history: string[];
+  history: Array<Array<string>>;
 }
 
 export interface ChatResponseBody {
@@ -25,7 +28,21 @@ export interface ChatResponseError {
  * probably pinecone is not yet working with the app directory
  */
 export async function POST(request: NextRequest) {
-  const { question, history } = await request.json() as RequestBody;
+  const { question, history } = (await request.json()) as RequestBody;
+
+  // fix TypeError: chatMessage._getType is not a function
+  // solution found here https://github.com/hwchase17/langchainjs/issues/1573#issuecomment-1582636486
+  const chatHistory: BaseChatMessage[] = [];
+  history?.forEach((_, idx) => {
+    if (!!history[idx]?.length) {
+      // first message is always human message
+      chatHistory.push(new HumanChatMessage(history[idx]?.[0] as string));
+    }
+    if (!!history[idx]?.length) {
+      // second message is always AI response
+      chatHistory.push(new AIChatMessage(history[idx]?.[1] as string));
+    }
+  });
 
   try {
     // const pineconeClient = pinecone;
@@ -42,10 +59,12 @@ export async function POST(request: NextRequest) {
     // });
 
     // res.status(200).json(response);
-    return NextResponse.json({ /* response */ });
+    return NextResponse.json({
+      /* response */
+    });
   } catch (error: any) {
-    console.log('error creating chain', error);
+    console.log("error creating chain", error);
     // res.status(500).json({ error: error.message || 'Something went wrong' });
-    return NextResponse.json({ error: 'Something went wrong' });
+    return NextResponse.json({ error: "Something went wrong" });
   }
 }
