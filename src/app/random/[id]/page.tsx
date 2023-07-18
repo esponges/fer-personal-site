e3d1659b960b62e79ee785d592dee53b1a6df8ab
esponges <esponges@gmail.com>
2023-07-18 14:50:15 -0600

    unused test route

--- src/app/random/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page({ params }: { params: { id: string } }) {
-  return (
-    <div>
-      <h1>Random Page</h1>
-      <p>Params: {JSON.stringify(params)}</p>
-    </div>
-  );
-}
