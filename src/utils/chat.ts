c29542b49f41d74617493fbc34668491349e2fd4
esponges <esponges@gmail.com>
2023-07-22 09:44:00 -0600

    add error handling for connecting to docs db

--- src/utils/chat.ts
+++ src/utils/chat.ts
@@ -14,6 +14,7 @@ import type { Document } from "langchain/document";
 import type { Doc } from "~/types";
 
 import * as schema from "../../drizzle/schema";
+import { getErrorMessage } from "./misc";
 
 const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
@@ -65,7 +66,12 @@ const client = new Client({
 });
 
 const connect = async () => {
-  await client.connect();
+  try {
+    await client.connect();
+  } catch (error) {
+    const errorMessage = getErrorMessage(error);
+    throw new Error(`error initializing docs db: ${errorMessage}`); 
+  }
 };
 
 connect();
