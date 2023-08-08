48c9bb37fafd6cfd428ad0a8a301c6bed0a913e6
esponges <esponges@gmail.com>
2023-08-08 15:22:22 -0600

    some margin to container

--- src/components/organisms/container.tsx
+++ src/components/organisms/container.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Container = ({ children, textCenter = true }: { children: React.ReactNode; textCenter?: boolean }) => {
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div className="mx-auto max-w-7xl my-6 md:my-4 px-4 sm:px-6 lg:px-8">
       <div className={`mx-auto mt-10 max-w-3xl ${textCenter ? "text-center" : ""}`}>{children}</div>
     </div>
   );
