ab7ce56c136599c434e4db2006f8aa75aef8540c
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2023-11-11 10:13:27 -0600

    check if assistant works in serverless

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { getErrorMessage } from "~/utils/misc";
 import { makeChain, makeStore } from "~/utils/chat";
+import OpenAI from "openai";
 
 import { type NextRequest, NextResponse } from "next/server";
 
@@ -9,12 +10,32 @@ interface RequestBody {
   history: Array<Array<string>>;
 }
 
+// Create a OpenAI connection
+const secretKey = process.env.OPENAI_API_KEY;
+const openai = new OpenAI({
+  apiKey: secretKey,
+});
+
 export async function POST(request: NextRequest) {
   const { question, history } = (await request.json()) as RequestBody;
 
   try {
     const HNSWStore = await makeStore();
     const chain = await makeChain(HNSWStore);
+
+    const assistant = await openai.beta.assistants.create({
+      name: "Math Tutor",
+      instructions:
+        "You are a personal math tutor. Write and run code to answer math questions.",
+      tools: [{ type: "code_interpreter" }],
+      model: "gpt-4-1106-preview",
+    });
+
+    // Log the first greeting
+    console.log(
+      "\nHello there, I'm your personal math tutor. Ask some complicated questions.\n"
+    );
+
 
     const sanitizedQuestion = question.trim().replaceAll("\n", " ");
     const chatHistoryAsString = history.map((msg) => msg.join("\n")).join("\n");
@@ -28,7 +49,8 @@ export async function POST(request: NextRequest) {
     });
 
     return NextResponse.json({
-      response,
+      ...response,
+      assistantId: assistant.id,
     });
   } catch (error) {
     const errorMsg = getErrorMessage(error);
