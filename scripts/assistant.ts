b74a940df327ff27e22525b55bc1fcc8270dad0c
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2023-11-13 12:35:21 -0600

    start w quiz then keep conversations

--- scripts/assistant.ts
+++ scripts/assistant.ts
@@ -16,7 +16,7 @@ const openai = new OpenAI({
   apiKey: secretKey,
 });
 
-async function askQuestion(question: string) {
+async function askRLineQuestion(question: string) {
   return new Promise<string>((resolve, _reject) => {
     rl.question(question, (answer: string) => {
       resolve(answer);
@@ -24,37 +24,33 @@ async function askQuestion(question: string) {
   });
 }
 
-function getMockResponseFromUserMultipleChoice() {
-  return "a";
-}
-
-function getMockResponseFromUserFreeResponse() {
-  return "I don't know";
-}
-
-function displayQuiz(title: string, questions: Record<string, string>[]) {
+// initial reference for implementation (in Pytho)
+// https://github.com/openai/openai-cookbook/blob/main/examples/Assistants_API_overview_python.ipynb
+async function displayQuiz(title: string, questions: Record<string, string>[]) {
   console.log("Quiz :", title);
   const responses = [];
 
   for (const question of questions) {
-    console.log("question_test string :", question["question_text"]);
     let response = "";
 
     // if multiple choice, print options
     if (question["question_type"] === "MULTIPLE_CHOICE") {
-      if (question["choices"]) {
-        for (const choices of question["choices"]) {
-          console.log(choices);
-        }
-      }
-      response = getMockResponseFromUserMultipleChoice();
+      const rLineQn = `Question: ${question["question_text"]}\n
+      Options: ${question["choices"]}\n
+      `;
+
+      response = await askRLineQuestion(rLineQn);
+
     } else if (question["question_type"] === "FREE_RESPONSE") {
-      response = getMockResponseFromUserFreeResponse();
+      const rLineQn = `Question: ${question["question_text"]}\n
+      `;
+
+      response = await askRLineQuestion(rLineQn);
     }
 
     responses.push(response);
   }
-  console.log("responses from quiz :", responses);
+  console.log("Your responses from the quiz :\n", responses);
   return responses;
 }
 
@@ -88,6 +84,8 @@ const quizJson = {
   },
 };
 
+let isQuizAnswered = false;
+
 async function main() {
   // const fileContent = fs.createReadStream("../public/pdf/test.txt");
 
@@ -110,31 +108,32 @@ async function main() {
           function: quizJson,
         },
       ],
-      // model: "gpt-4-1106-preview",
-      model: "gpt-3.5-turbo-1106",
+      model: "gpt-4-1106-preview",
+      // model: "gpt-3.5-turbo-1106",
     });
 
     // Log the first greeting
     console.log(
-      "\nHello there, I'm Fernando's personal Math assistant. Ask some questions abot Fer.\n",
+      "\nHello there, I'm Fernando's personal Math assistant. We'll start with a small quiz.\n",
     );
 
     // Create a thread
     const thread = await openai.beta.threads.create();
 
-    console.log("starting thread with id: ", thread.id);
-
     // Use keepAsking as state for keep asking questions
     let keepAsking = true;
 
     while (keepAsking) {
-      const userQuestion = await askQuestion("\nWhat is your question? ");
-      
+      const userQuestion = isQuizAnswered
+        ? await askRLineQuestion("You next question to the model: \n")
+        // this will make the model  build a quiz using our provided function
+        : "Make a quiz with 2 questions: One open ended, one multiple choice" +
+          "Then, give me feedback for the responses.";
+
       // Pass in the user question into the existing thread
       await openai.beta.threads.messages.create(thread.id, {
         role: "user",
         content: userQuestion,
-        // file_ids: [file.id],
       });
 
       // Use runs to wait for the assistant response and then retrieve it
@@ -165,12 +164,13 @@ async function main() {
           const args = JSON.parse(toolCall?.function?.arguments || "{}");
           const questions = args.questions;
 
-          const responses = displayQuiz(
-            name || "cool quiz",
-            questions,
-          );
+          const responses = await displayQuiz(name || "cool quiz", questions);
 
-          const reRun = await openai.beta.threads.runs.submitToolOutputs(
+          // toggle flag that sets initial quiz
+          isQuizAnswered = true;
+
+          // we must submit the tool outputs to the run to continue
+          await openai.beta.threads.runs.submitToolOutputs(
             thread.id,
             run.id,
             {
@@ -182,15 +182,11 @@ async function main() {
               ],
             },
           );
-
-          console.log("requires_action - reRun :", reRun);
         }
-
         // keep polling until the run is completed
         await new Promise((resolve) => setTimeout(resolve, 2000));
         actualRun = await openai.beta.threads.runs.retrieve(thread.id, run.id);
       }
-      console.log("thinking...");
 
       // Get the last assistant message from the messages array
       const messages = await openai.beta.threads.messages.list(thread.id);
@@ -214,8 +210,8 @@ async function main() {
       }
 
       // Then ask if the user wants to ask another question and update keepAsking state
-      const continueAsking = await askQuestion(
-        "Do you want to ask another question? (yes/no) ",
+      const continueAsking = await askRLineQuestion(
+        "Do you want to keep having a conversation? (yes/no) ",
       );
 
       keepAsking = continueAsking.toLowerCase() === "yes";
