88b3c3f0fd023545a57b2e344e0c710569feed88
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2023-12-28 15:39:28 -0600

    add container and allow extra classNames

--- src/components/organisms/container.tsx
+++ src/components/organisms/container.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
-export const Container = ({ children, textCenter = true }: { children: React.ReactNode; textCenter?: boolean }) => {
+export const Container = ({
+  children,
+  textCenter = true,
+  classNames,
+}: {
+  children: React.ReactNode;
+  textCenter?: boolean;
+  classNames?: string;
+}) => {
   return (
     <div className="mx-auto max-w-7xl my-6 md:my-4 px-4 sm:px-6 lg:px-8">
-      <div className={`mx-auto mt-10 max-w-3xl ${textCenter ? "text-center" : ""}`}>{children}</div>
+      <div
+        className={twMerge(
+          "mx-auto max-w-3xl",
+          textCenter ? "text-center" : "",
+          classNames,
+        )}
+      >
+        {children}
+      </div>
     </div>
   );
 };
