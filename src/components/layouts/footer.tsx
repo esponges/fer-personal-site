bfa26154fec51fd1d281ff41f5d02cd031d03c43
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-07-24 18:22:12 -0600

    fix heading levels

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -19,16 +19,15 @@ export const Footer = () => {
         <div className="mx-6 text-center md:py-2 md:text-left">
           <div className="grid-1 grid grid-cols-2 gap-8">
             <div className="">
-              <h6
+              <h1
                 className="
-            mb-4
-            flex
-            items-center
-          
-            justify-end
-            font-semibold
-            uppercase
-          "
+                  mb-4
+                  flex
+                  items-center
+                  justify-end
+                  font-semibold
+                  uppercase
+                "
               >
                 <svg
                   aria-hidden="true"
@@ -46,7 +45,7 @@ export const Footer = () => {
                   ></path>
                 </svg>
                 Fer Tostado Web Dev
-              </h6>
+              </h1>
               <p className="justify-end text-right">
                 This is my personal website and I don&apos;t intend to make any
                 money off of it. I just want to share my passion for web
@@ -54,9 +53,9 @@ export const Footer = () => {
               </p>
             </div>
             <div className="">
-              <h6 className="mb-4 flex justify-end font-semibold uppercase">
+              <h1 className="mb-4 flex justify-end font-semibold uppercase">
                 Contact
-              </h6>
+              </h1>
               <p className="mb-4 flex items-center justify-end">
                 <svg
                   aria-hidden="true"
@@ -70,7 +69,7 @@ export const Footer = () => {
                 >
                   <path
                     fill="currentColor"
-                    d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"
+                    d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h14a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"
                   ></path>
                 </svg>
                 Guadalajara, Jalisco, MX
